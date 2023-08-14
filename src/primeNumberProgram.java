9d7a90c84d99f1d3838551465463fb539d3fc1d4
surapath <Suraj.Pathak@amdocs.com>
2023-08-14 21:32:27 +0530

    updated few more practise codes

--- src/primeNumberProgram.java
+++ src/primeNumberProgram.java
@@ -3,6 +3,14 @@ import java.util.Scanner;
 public class primeNumberProgram {
 
     public static void main(String[] args) {
+
+
+        System.out.println(isPrimeNumber(20));
+        getPrimeNumbers(10);
+        primeNumber();
+    }
+
+    public static void primeNumber(){
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter Number : ");
         int number = scan.nextInt();
@@ -15,6 +23,7 @@ public class primeNumberProgram {
 
         if (number == 0 || number == 1) {
             System.out.println("Number is not prime");
+           flag = 1;
         } else
             for (int i = 2; i < m; i++) {
                 if (number % i == 0) {
@@ -25,6 +34,36 @@ public class primeNumberProgram {
             }
         if (flag == 0) {
             System.out.println(number + " is prime number");
+        }
+    }
+
+    // Another Way
+
+    public static boolean isPrimeNumber(int num){
+        // edge cornet cases , 0 and 1 are not prime numbers , lowest prime number is 2 , negative are not prime numbers
+        if (num<=1){
+            return false;
+        }
+
+        for(int i =2;i<num;i++){
+            if(num % i == 0){
+                return false;
+            }
+
+        }
+        return  true;
+
+    }
+
+    // now to print all prime numbers upto particular number
+
+    public static void getPrimeNumbers(int num){
+        System.out.println("Prime numbers upto : "+num);
+        System.out.println();
+        for (int i=2;i<=num;i++){
+            if (isPrimeNumber(i))
+                System.out.println(i + "");
+
         }
     }
 
